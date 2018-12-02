b577b4f50b156630bdb85863a6eb5ca916fd1a5d
panwei <panwei@lianjia.com>
2018-12-02 16:44:52 +0800

    add network img

--- app/src/main/java/android/com/anxintao/goodDetail/GoodDetailActivity.java
+++ app/src/main/java/android/com/anxintao/goodDetail/GoodDetailActivity.java
@@ -1,12 +1,16 @@
 package android.com.anxintao.goodDetail;
 
 import android.app.Activity;
+import android.com.anxintao.AnxintaoApplication;
 import android.com.anxintao.R;
 import android.com.anxintao.tools.ToastUtil;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import com.oragee.banners.BannerView;
+import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +22,7 @@ public class GoodDetailActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_good_detail);
     initBannerView();
+    initPicListView();
   }
 
   private void initBannerView() {
@@ -37,4 +42,35 @@ public class GoodDetailActivity extends Activity {
     mBannerView.setViewList(imageList);
     mBannerView.startLoop(true);
   }
+
+  private void initPicListView(){
+    LinearLayout picListContainer = findViewById(R.id.ll_img_container);
+    //ImageView imageView = findViewById(R.id.iv_pic);
+    //Picasso.with(AnxintaoApplication.getInstance()).load("http://img.hb.aicdn.com/5b591216952133a8b45b8fcba09c7f9f8c4731927531-Tj1dsT_fw658").into(imageView);
+    //  <!--<ImageView-->
+    //        <!--android:id="@+id/iv_pic"-->
+    //        <!--android:adjustViewBounds="true"-->
+    //        <!--android:scaleType="fitXY"-->
+    //        <!--android:layout_width="match_parent"-->
+    //        <!--android:layout_height="wrap_content"/>
+
+    for (int i = 0; i <5 ; i++) {
+      ImageView imageView = new ImageView(this);
+      imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+      imageView.setAdjustViewBounds(true);
+      imageView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+          ViewGroup.LayoutParams.WRAP_CONTENT));
+      Picasso.with(AnxintaoApplication.getInstance()).load("http://img.hb.aicdn.com/5b591216952133a8b45b8fcba09c7f9f8c4731927531-Tj1dsT_fw658").into(imageView);
+      picListContainer.addView(imageView);
+    }
+
+
+
+
+  }
+
+
+
+
+
 }
