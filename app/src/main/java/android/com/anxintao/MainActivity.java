6d8054a5295ecf82e364afb0b677c989b59c90e2
panwei <panwei@lianjia.com>
2018-12-23 23:04:25 +0800

    add my store ui

--- app/src/main/java/android/com/anxintao/MainActivity.java
+++ app/src/main/java/android/com/anxintao/MainActivity.java
@@ -5,14 +5,18 @@ import android.com.anxintao.home.HomeFragment;
 import android.com.anxintao.store.StoreFragment;
 import android.com.anxintao.user.UserFragment;
 import android.com.anxintao.widget.NoScrollViewPager;
+import android.content.Context;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -45,9 +49,60 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     super.onCreate(savedInstanceState);
     requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.activity_main);
+    //initWindows();
     initViews();//初始化控件
     initEvents();//初始化事件
     initDatas();//初始化数据
+  }
+
+  private void initWindows() {
+    Window window = getWindow();
+    int color = getResources().getColor(android.R.color.transparent);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+          | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+      window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+          | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+          | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+      //设置状态栏颜色
+      window.setStatusBarColor(color);
+      //设置导航栏颜色
+      window.setNavigationBarColor(color);
+      ViewGroup contentView = ((ViewGroup) findViewById(android.R.id.content));
+      View childAt = contentView.getChildAt(0);
+      if (childAt != null) {
+        childAt.setFitsSystemWindows(true);
+      }
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      //透明状态栏
+      window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+      //透明导航栏
+      window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+      //设置contentview为fitsSystemWindows
+      ViewGroup contentView = (ViewGroup) findViewById(android.R.id.content);
+      View childAt = contentView.getChildAt(0);
+      if (childAt != null) {
+        childAt.setFitsSystemWindows(true);
+      }
+      //给statusbar着色
+      View view = new View(this);
+      view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight(this)));
+      view.setBackgroundColor(color);
+      contentView.addView(view);
+    }
+  }
+
+  /**
+   * 获取状态栏高度
+   *
+   * @param context context
+   * @return 状态栏高度
+   */
+  private static int getStatusBarHeight(Context context) {
+    // 获得状态栏高度
+    int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+    return context.getResources().getDimensionPixelSize(resourceId);
   }
 
   private void initDatas() {
