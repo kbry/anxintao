43807a59efb8c5690e4a32aef7659c441f8f26ee
panwei <panwei@lianjia.com>
2018-12-24 01:07:32 +0800

    add store ui

--- app/src/main/java/android/com/anxintao/store/StoreFragment.java
+++ app/src/main/java/android/com/anxintao/store/StoreFragment.java
@@ -1,13 +1,27 @@
 package android.com.anxintao.store;
 
 import android.com.anxintao.R;
+import android.com.anxintao.goodDetail.GoodDetailActivity;
+import android.com.anxintao.tools.ToastUtil;
+import android.com.anxintao.widget.OnItemClickListener;
+import android.com.anxintao.widget.RecycleViewDivider;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StoreFragment extends Fragment {
+
+  private View rootView;
+
+  private RecyclerView mList;
+
   public StoreFragment() {
   }
   @Override public void onCreate(Bundle savedInstanceState) {
@@ -15,7 +29,33 @@ public class StoreFragment extends Fragment {
   }
 
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_store, container, false);
+    rootView = inflater.inflate(R.layout.fragment_store, container, false);
+    initList();
+    return rootView;
+  }
+
+  private void initList(){
+    mList = rootView.findViewById(R.id.rv_list);
+    LinearLayoutManager
+        layout = new LinearLayoutManager(this.getContext(),LinearLayoutManager.VERTICAL,false);
+    layout.setSmoothScrollbarEnabled(true);
+    layout.setAutoMeasureEnabled(true);
+    mList.setLayoutManager(layout);
+    mList.addItemDecoration(new RecycleViewDivider(StoreFragment.this.getContext(),LinearLayoutManager.HORIZONTAL,45,getResources().getColor(android.R.color.white)));
+    List<String> test = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      test.add(i+"test");
+    }
+    StoreOwnerItemAdapter adapter = new StoreOwnerItemAdapter(test);
+    adapter.setOnViewItemClickListener(new OnItemClickListener() {
+      @Override public void onItemClick(View view, int position) {
+        ToastUtil.toast("---"+position);
+        Intent intent = new Intent(StoreFragment.this.getContext(), GoodDetailActivity.class);
+        intent.putExtra("goodid",position);
+        startActivity(intent);
+      }
+    });
+    mList.setAdapter(adapter);
   }
 
 }
---
+++ app/src/main/java/android/com/anxintao/store/StoreOwnerItemAdapter.java
@@ -0,0 +1,64 @@
+package android.com.anxintao.store;
+
+import android.com.anxintao.R;
+import android.com.anxintao.widget.OnItemClickListener;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.List;
+
+/**
+ * 创建时间: 2018/12/01 17:47 <br>
+ * 作者: panwei <br>
+ * 描述:
+ */
+
+public class StoreOwnerItemAdapter extends RecyclerView.Adapter<StoreOwnerItemAdapter.ViewHolder>{
+
+  private List<String> list;
+
+  private OnItemClickListener mOnItemClickListener;
+  public StoreOwnerItemAdapter(List<String> list) {
+    this.list = list;
+  }
+
+  public void setOnViewItemClickListener(OnItemClickListener onItemClickListener){
+    this.mOnItemClickListener = onItemClickListener;
+  }
+
+  @Override
+  public StoreOwnerItemAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.store_owner_listview_item, parent, false);
+    StoreOwnerItemAdapter.ViewHolder viewHolder = new StoreOwnerItemAdapter.ViewHolder(view);
+    viewHolder.mItemView.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        mOnItemClickListener.onItemClick(v,(int)v.getTag());
+      }
+    });
+    return viewHolder;
+  }
+
+  @Override
+  public void onBindViewHolder(StoreOwnerItemAdapter.ViewHolder holder, int position) {
+    //holder.mText.setText(list.get(position));
+    holder.mItemView.setTag(position);
+  }
+
+  @Override
+  public int getItemCount() {
+    return list.size();
+  }
+
+  class ViewHolder extends RecyclerView.ViewHolder {
+    TextView mText;
+    View mItemView;
+    ViewHolder(View itemView) {
+      super(itemView);
+      mItemView = itemView;
+      //mText = itemView.findViewById(R.id.tv_type);
+    }
+  }
+
+}
